50c4858b53667e45040533f3a54d03982847ac9d
BiwenTao <wbt199794@163.com>
2021-09-13 11:18:31 +0800

    project 1b

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -14,6 +14,10 @@ public class TestOffByOne {
         assertFalse(offByOne.equalChars('a', 'e'));
         assertFalse(offByOne.equalChars('z', 'a'));
         assertFalse(offByOne.equalChars('a', 'a'));
+        assertFalse(offByOne.equalChars('b', 'A'));
+        assertTrue(offByOne.equalChars('&', '%'));
+        assertFalse(offByOne.equalChars('&', '.'));
+
 
     }
 
